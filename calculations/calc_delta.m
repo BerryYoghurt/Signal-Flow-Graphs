f73f82a7e89c9394a7b30f8092b00516f539d32d
Manar Amr <59005827+ManarAmr25@users.noreply.github.com>
2021-05-05 06:06:34 +0200

    calc_delta tested. needs more testing

--- calculations/calc_delta.m
+++ calculations/calc_delta.m
@@ -1,11 +1,31 @@
 function d = calc_delta(G)   %this function calculates delta for any given graph
-   loops_list = loops(G); 
-   i = 1;
-   non_touching_loops = combinations(G, loops_list, i+1); %TODO: combinations function is not implementes yet
-   d = 1;
-   while length(non_touching_loops) ~= 0
-      product = %TODO: what is the format of the list non_touching_loops?
-      d = d + ((-1)^i)*product;
+   d = 1; %delta
+   loops_list = loop(G) %returns a list of all individual loops
+   celldisp(loops_list);
+   %p >> list of gains of individual loops
+   p = forward_path_gain(G, loops_list) %TODO: change function name to be general
+   for x = p %add values of individual loops to delta
+      d = d - x; 
    end
+   %----------------------------------------------------------------------------------
+   i = 2; %start form groups of 2 non-touching loops
+   non_touching_loops = Non_Touching(i,loops_list) %returns a list of i non-touching loops
+   celldisp(non_touching_loops);
+   while length(non_touching_loops) ~= 0 %keep adding values to delta until non_touching loops list is empty
+        for comb = non_touching_loops %one possible combination
+            celldisp(comb{1});
+            product = 1; %multiply the gains of each group. result in product
+            %TODO: change function name to be general
+            l = forward_path_gain(G,comb{1}); %calculate gains of all loops to get their product
+            for val = l %one path in the combination
+                product = product * val; %TODO: change function name to be general
+            end
+            d = d + ((-1)^i) * product; %add product to delta
+        end
+        %prepare for the next iteration
+        i = i + 1;
+        non_touching_loops = Non_Touching(i, loops_list); %returns a list of i non-touching loops
+   end
+   
    return
 end
--- calculations/forward_path_gain.m
+++ calculations/forward_path_gain.m
@@ -1,16 +1,16 @@
 function x = forward_path_gain(G, p)
     %G >> digraph object
     %p >> list of all forward paths
-    arr = table2array(G.Edges)
+    arr = table2array(G.Edges);
     x = [];
     for i = 1:length(p)
-        vec = p{i}
+        vec = p{i};
         product = 1;
         for j = 1:length(vec)-1
             for k = 1:size(arr,1) 
-                arr(k,1)
-                arr(k,2)
-                arr(k,3)
+                arr(k,1);
+                arr(k,2);
+                arr(k,3);
                 if arr(k,1) == vec(j) && arr(k,2) == vec(j+1)
                     product = product * arr(k,3);
                 end
