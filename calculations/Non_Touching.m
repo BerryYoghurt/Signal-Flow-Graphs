6c146b8e6485801e0c12f4e6fab05b3e05088b39
nourwalid <58997274+nourwalid70@users.noreply.github.com>
2021-05-05 02:22:41 +0200

    Add files via upload

---
+++ calculations/Non_Touching.m
@@ -0,0 +1,27 @@
+function NonL = Non_Touching(i,C) %i>1   , C = loop(G);
+% [ [ 1 2 3 1] [4 5 6 4]  [ 4 8 4] ]  1 2 3 1   4 5 6 4
+NonL = { };   
+combos = combntns(C,i);
+celldisp(combos);
+rows = size(combos,1)
+col= size(combos,2)
+count = 1;
+for  k = 1:rows 
+   flagg=0;
+for  n = 1:col 
+arr1 = combos{k,n}
+check = zeros(size(arr1));
+  for m = n+1:col
+  arr2= combos{k,m}
+  arr = ismember(arr1,arr2);
+   if(isequal(arr,check) == 0) 
+       flagg=1;
+       break;
+   end
+  end
+ end
+ if flagg==0 
+ NonL{count}= combos(k,:);
+ count = count+1;
+ end
+end
---
+++ calculations/loop.m
@@ -0,0 +1,40 @@
+function c = loop(g)
+%FINDCYCLES Finds all simple cycles in graph G
+% C = FINDCYCLES(G) returns a cell array containing all simple cyclesFind
+
+c = {};
+
+for i=1:numnodes(g)
+    % Find all cycles starting with node ii, which only contain nodes
+    % with indices >= i
+    c = findcycleRecursive(i, g, c, i);
+end
+
+end
+
+
+function c = findcycleRecursive(currentPath, g, c, minNodeID)
+
+    successorLast = reshape(successors(g, currentPath(end)), 1, []);
+
+for s = successorLast
+    if s < minNodeID
+        continue;
+    end
+    
+    if s == currentPath(1)
+        % Found a cycle
+        c{end+1} = [currentPath s]; %#ok<AGROW>
+    elseif any(currentPath == s)
+        % Contains a subcycle, this would result in a cycle that is not
+        % simple at best.
+    else
+        % Add node s and continue growing the path.
+        c = findcycleRecursive([currentPath s], g, c, minNodeID);
+    end
+end
+
+end
+
+
+
