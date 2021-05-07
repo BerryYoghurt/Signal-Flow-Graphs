14d61af72fe4852b941380f4a5ebe471951685f6
Manar Amr <59005827+ManarAmr25@users.noreply.github.com>
2021-05-07 17:58:29 +0200

    Add files via upload

--- calculations/eleminate_path.m
+++ calculations/eleminate_path.m
@@ -1,9 +1,6 @@
-function G1 = eleminate_path(G, p)   %this function calculates delta for any given graph
+function G1 = eleminate_path(G, p)
     %G >> a digraph object
     %p >> vector representing path
-    %from = p(1:length(p)-1)
-    %to = p(2:length(p))
-    %G1 = rmedge(G,from,to); %Q: should i eleminate only edges or eleminate nodes too ?
     G1 = rmnode(G,p);
     return
 end
