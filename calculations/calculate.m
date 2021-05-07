3f71ccebf67a6358408df5c8fe7c9ec4865e95e9
mariamsaed19 <58952137+mariamsaed19@users.noreply.github.com>
2021-05-07 17:07:07 +0200

    Add files via upload

--- calculations/calculate.m
+++ calculations/calculate.m
@@ -8,9 +8,10 @@ display('forward paths list');
 celldisp(list);
 x = calc_gains(G, list);
 %d = calc_delta(G);
-delta_list = delta(G,list)
-result = transfer_function(x, delta_list)
+delta_list = delta(G,list);
+transfer_function_value = transfer_function(x, delta_list)
 
 diary off;
 show_steps =fileread('files');
+%show_steps= 'hi';
 end
